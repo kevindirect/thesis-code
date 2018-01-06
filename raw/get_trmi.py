849e7fa074c934d0621e520cbf830797d35ea250
Kevin <kpatelprime@gmail.com>
2018-01-06 07:58:31 -0800

    changed get_trmi

--- raw/get_trmi.py
+++ raw/get_trmi.py
@@ -5,24 +5,32 @@ import pandas as pd
 import datetime
 import sys
 import getopt
-from os import getcwd, sep, path, makedirs
-sys.path.insert(0, path.abspath('..'))
-from common import *
+from os import getcwd, sep, path, makedirs, pardir
+import json
+sys.path.insert(0, path.abspath(pardir))
+from common import makedir_if_not_exists
 
 def main(argv):
-	usage = lambda: print('get_trmi.py -v <version> [-k <keep_ns>]')
-	ver = None
-	valid_ver = ['v2', 'v3']
+	usage = lambda: print('get_trmi.py [-p <pathsfile> -k -t]')
+	pfx = getcwd() +sep
+	trmi_info_dir = path.abspath(pardir) +sep
 
-	# Default Parameters / login info
-	url = 'https://api.marketpsych.com'
-	key = 'cus_BgEA801LPsAccR'
-	per = 'hourly'   # daily, hourly, or minutely
-	keep_ns = False  # Set True to keep News_Social data in result
-	startend = {'start': '1998-01-01', 'end':'2017-12-23'}
+	# trmi.json file contains api url and key
+	if (path.isfile(trmi_info_dir +'trmi.json')):
+		with open(trmi_info_dir +'trmi.json') as json_data:
+			trmi = json.load(json_data)
+	else:
+		print('trmi.json must be present in the following directory:', trmi_info_dir)
+		sys.exit(2)
+
+	# Default Parameters
+	per = 'hourly'   			# daily, hourly, or minutely
+	pathsfile = 'paths.json'
+	keep_ns = False
+	startend = {'start': '1998-01-01', 'end':'2018-01-07'}
 
 	try:
-		opts, args = getopt.getopt(argv,'hv:k:', ['help', 'version=', 'keep_ns='])
+		opts, args = getopt.getopt(argv,'hp:kt', ['help', 'pathsfile=', 'keep_ns', 'test'])
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
@@ -31,109 +39,80 @@ def main(argv):
 		if opt in ('-h', '--help'):
 			usage()
 			sys.exit()
-		elif opt in ('-v', '--version'):
-			ver = arg
-			if ver not in valid_ver:
-				print('invalid version - the following are valid versions:', ', '.join(valid_ver))
-				sys.exit(2)
+		elif opt in ('-p', '--pathsfile'):
+			pathsfile = arg
 		elif opt in ('-k', '--keep_ns'):
-			if (arg == 'True'):
-				keep_ns = True
-			elif (arg == 'False'):
-				keep_ns = False
-			else:
-				print('keep_ns must be a True or False string')
-				sys.exit(2)
+			keep_ns = True
+		elif opt in ('-t', '--test'):
+			startend = {'start': '2015-08-01', 'end':'2015-08-03'}
 
-	if (not ver):
-		print('need to specify a version')
-		usage()
+	# paths.json tells script what to pull from the api and where to put it
+	if (path.isfile(pfx +pathsfile)):
+		with open(pfx +pathsfile) as json_data:
+			trmi_paths = json.load(json_data)['trmi']
+	else:
+		print(pathsfile, ' must be present in the current directory')
 		sys.exit(2)
-
-	asset_lists_v2 = {
-		'etf': ['MPTRXUS30', 'MPTRXUS500', 'MPTRXUSMID2000', 'MPTRXUSNAS100'], # CMPNY_GRP
-		'enm' : ['CRU', 'GOL'] # COM_ENM
-	}
-	asset_lists_v3 = {
-		'coumkt': ['US'], # COU_MKT
-		'etf': ['MPTRXUS30', 'MPTRXUS500', 'MPTRXUSMID2000', 'MPTRXUSNAS100'], # CMPNY_GRP
-		'enm' : ['CRU', 'GOL'] # COM_ENM
-	}
-	asset_lists = asset_lists_v2 if ver == 'v2' else asset_lists_v3
-
-	def make_csv_group_request_url(group, assets, period=per, times=startend, version=ver, start_url=url, apikey=key):
-		reqlist = []
-		params = []
-
-		params.append(period +'?apikey=' +apikey)
-		params.append('assets=' +','.join(assets))
-		params.append('start=' +times['start'])
-		params.append('end=' +times['end'])
-		params.append('csv=1')
-
-		reqlist.append(start_url)
-		reqlist.append('trmi')
-		reqlist.append(version)
-		reqlist.append('data')
-		reqlist.append(group)
-		reqlist.append('&'.join(params))
-
-		return '/'.join(reqlist)
 
 	dropfirst = ['id', 'Date', 'Asset']
 	join_cols = ['assetCode', 'windowTimestamp']
+	for ver, groups in trmi_paths.items():
+		for group, assets in groups.items():
+			endpoint = make_csv_group_request_url(group, assets, ver, per, startend, trmi['api']['url'], trmi['api']['key'])
+			print(group, endpoint)
+			df = pd.read_csv(endpoint)
+			dir_path = pfx +'trmi' +sep +ver +sep +group
+			makedir_if_not_exists(dir_path)
 
-	for group, assets in asset_lists.items():
-		endpoint = make_csv_group_request_url(group, assets)
-		print(group, endpoint)
-		df = pd.read_csv(endpoint)
-		dir_path = getcwd() +sep +'trmi' +ver +sep +group
-		makedir_if_not_exists(dir_path)
-		# with open(dir_path +sep +'metadata.txt', 'a') as text_file:
-		# 	text_file.write('asset: ' +str(df['Asset'].unique()) +'\n')
-		# 	text_file.write('group: ' +group +'\n')
-		# 	text_file.write('version: ' +str(df['systemVersion'].unique()) +'\n')
-		# 	text_file.write('\n')
-
-		df = df.drop(dropfirst, axis=1, errors='ignore')
-		news = df.loc[df['dataType'] == 'News'].drop('dataType', axis=1)
-		social = df.loc[df['dataType'] == 'Social'].drop('dataType', axis=1)
-		merged = news.merge(social, on=join_cols, suffixes=('_N', '_S'))
-		if (keep_ns):
-			news_social = df.loc[df['dataType'] == 'News_Social'].drop('dataType', axis=1)
-			merged = merged.merge(news_social, on=join_cols, suffixes=('', '_NS'))
-
-		for asset in assets:
-			print('\t' +asset, end='')
-			asset_df = merged.loc[merged['assetCode'] == asset]
-
-			datehourmin_col = asset_df['windowTimestamp'].map(lambda w: w[:16])
-			date_col = asset_df['windowTimestamp'].map(lambda w: w[:10])
-			hour_col = asset_df['windowTimestamp'].map(lambda w: int(w[11:13]))
-			dow_col = date_col.map(lambda w: datetime.datetime.strptime(w, '%Y-%m-%d').weekday())
-			day_col = asset_df['windowTimestamp'].map(lambda w: int(w[8:10]))
-			month_col = asset_df['windowTimestamp'].map(lambda w: int(w[5:7]))
-			year_col = asset_df['windowTimestamp'].map(lambda w: int(w[:4]))
-
-			asset_df.insert(0, 'id', datehourmin_col)
-			asset_df.insert(1, 'date', date_col)
-			asset_df.insert(2, 'hour', hour_col)
-			asset_df.insert(3, 'dow', dow_col)
-			asset_df.insert(4, 'day', day_col)
-			asset_df.insert(5, 'month', month_col)
-			asset_df.insert(6, 'year', year_col)
-			asset_df.insert(7, 'news_ver', asset_df['systemVersion_N'])
-			asset_df.insert(8, 'social_ver', asset_df['systemVersion_S'])
-			asset_df = asset_df.drop(['systemVersion_N', 'systemVersion_S'], axis=1, errors='ignore')
+			df = df.drop(dropfirst, axis=1, errors='ignore')
+			news = df.loc[df['dataType'] == 'News'].drop('dataType', axis=1)
+			social = df.loc[df['dataType'] == 'Social'].drop('dataType', axis=1)
+			merged = news.merge(social, on=join_cols, suffixes=('_N', '_S'))
 			if (keep_ns):
-				asset_df.insert(9, 'news_social_ver', asset_df['systemVersion_NS'])
-				asset_df = asset_df.drop('systemVersion_NS', axis=1, errors='ignore')
+				news_social = df.loc[df['dataType'] == 'News_Social'].drop('dataType', axis=1)
+				merged = merged.merge(news_social, on=join_cols, suffixes=('', '_NS'))
 
-			asset_df = asset_df.drop(join_cols, axis=1)
-			asset_df = asset_df.set_index('id', drop=True).sort_index()
-			asset_df.to_csv(dir_path +sep +asset +'.csv')
-			print('...done')
-		print()
+			# Rename systemVersion and prefix all data columns with group name
+			merged.rename(columns={'systemVersion_N': 'n_ver', 'systemVersion_S': 's_ver', 'systemVersion': 'ver'}, inplace=True)
+			merged.columns = merged.columns.map(lambda s: str(group[-3:] +'_' +s) if s not in join_cols else s)
+
+			for asset in assets:
+				print('\t' +asset, end='')
+				asset_df = merged.loc[merged['assetCode'] == asset]
+
+				date_col = asset_df['windowTimestamp'].map(lambda w: w[:10])
+				asset_df.insert(0, 'id', asset_df['windowTimestamp'].map(lambda w: w[:16]))
+				asset_df.insert(1, 'date', date_col)
+				asset_df.insert(2, 'hour', asset_df['windowTimestamp'].map(lambda w: int(w[11:13])))
+				asset_df.insert(3, 'dow', date_col.map(lambda w: datetime.datetime.strptime(w, '%Y-%m-%d').weekday()))
+				asset_df.insert(4, 'day', asset_df['windowTimestamp'].map(lambda w: int(w[8:10])))
+				asset_df.insert(5, 'month', asset_df['windowTimestamp'].map(lambda w: int(w[5:7])))
+				asset_df.insert(6, 'year', asset_df['windowTimestamp'].map(lambda w: int(w[:4])))
+
+				asset_df = asset_df.drop(join_cols, axis=1)
+				asset_df = asset_df.set_index('id', drop=True).sort_index()
+				asset_df.to_csv(dir_path +sep +asset +'.csv')
+				print('...done')
+			print()
+
+def make_csv_group_request_url(group, assets, version, period, times, api_url, api_key):
+	reqlist = []
+	params = []
+
+	params.append(period +'?apikey=' +api_key)
+	params.append('assets=' +','.join(assets))
+	params.append('start=' +times['start'])
+	params.append('end=' +times['end'])
+	params.append('csv=1')
+
+	reqlist.append(api_url)
+	reqlist.append('trmi')
+	reqlist.append(version)
+	reqlist.append('data')
+	reqlist.append(group)
+	reqlist.append('&'.join(params))
+
+	return '/'.join(reqlist)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
