8b851323106ae0717cba9957e3f135603b5326a2
Kevin <kpatelprime@gmail.com>
2018-02-26 03:18:05 -0800

    changed defaults in /data/ and removed the now defunct run.sh from the subpackages

--- data/run.sh
+++
@@ -1,3 +0,0 @@
-source activate root
-python3 produce_data.py -s split_mocl.json
-source deactivate root
--- raw/run.sh
+++
@@ -1,4 +0,0 @@
-source activate root
-python3 get_price.py
-python3 get_trmi.py -k
-source deactivate root
