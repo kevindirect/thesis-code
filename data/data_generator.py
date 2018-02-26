01ac8954eeac13e7f87b4e697315fe468206c83d
Kevin <kpatelprime@gmail.com>
2018-02-25 22:29:41 -0800

    converted data_generator to new conventions

--- data/data_generator.py
+++ data/data_generator.py
@@ -1,55 +1,35 @@
 # Kevin Patel
 
-import numpy as np
 import pandas as pd
 import sys
 import getopt
-from os import getcwd, sep, path, makedirs, pardir
+from os import sep, path
 import json
 from functools import reduce
-sys.path.insert(0, path.abspath(pardir))
-from common_util import makedir_if_not_exists, inner_join, load_csv, get_subset
-
+from common import load_json, load_csv, makedir_if_not_exists, inner_join, get_subset
+from common import DATA_DIR, default_joinfile, default_splitfile, default_accessfile
 
 class DataGenerator:
-	pfx = getcwd() +sep
-
-	def __init__(self, joinfile='join.json', splitfile='split.json', accessfile='access.json'):
+	def __init__(self, joinfile=default_joinfile, splitfile=default_splitfile, accessfile=default_accessfile):
 		# joinfile enumerates the available assets to run on
-		if (path.isfile(DataGenerator.pfx +joinfile)):
-			with open(DataGenerator.pfx +joinfile) as json_data:
-				self.assets = list(json.load(json_data).keys())
-		else:
-			print(joinfile, 'must be present in the following directory:', DataGenerator.pfx)
-			sys.exit(2)
+		self.assets = list(load_json(joinfile, dir_path=DATA_DIR).keys())
 
 		# splitfile enumerates the available split groups and splits within them
-		if (path.isfile(DataGenerator.pfx +splitfile)):
-			with open(DataGenerator.pfx +splitfile) as json_data:
-				self.splits = json.load(json_data)
-		else:
-			print(splitfile, 'must be present in the following directory:', DataGenerator.pfx)
-			sys.exit(2)
+		self.splits = load_json(splitfile, dir_path=DATA_DIR)
 
 		# accessfile defines the group membership of each label group and the columns they can access
 		# It defines the default data that will be furnished by the generator
-		if (path.isfile(DataGenerator.pfx +accessfile)):
-			with open(DataGenerator.pfx +accessfile) as json_data:
-				self.access = json.load(json_data)
-		else:
-			print(accessfile, 'must be present in the current directory')
-			sys.exit(2)
+		self.access = load_json(accessfile)
 
 	def get_generator(self, asset_list=None, access_dict=None):
-		if (asset_list is None):
-			asset_list = self.assets
-		if (access_dict is None):
-			access_dict = self.access
+		asset_list = self.assets if (asset_list is None) else asset_list
+		access_dict = self.access if (access_dict is None) else access_dict
+
 		assert(all((asset in self.assets) for asset in asset_list))
 		assert(all((split_group in self.splits.keys()) for split_group in access_dict.keys()))
 
 		for split_group_name, split_group in access_dict.items():
-			split_group_dir = DataGenerator.pfx +split_group_name +sep
+			split_group_dir = DATA_DIR +split_group_name +sep
 
 			for asset in asset_list:
 				asset_dir = split_group_dir +asset +sep
@@ -59,13 +39,12 @@ class DataGenerator:
 					all_splits = [al['data_access'] for al in access_levels.values()]
 					split_set = list(dict.fromkeys(reduce(lambda a,b: a+b, all_splits)))
 					assert(all((split in self.splits[split_group_name]['#ASSET']) for split in split_set))
-					split_dfs = {split: load_csv(asset_dir +split +'.csv') for split in split_set}
-					label_df = load_csv(asset_dir +label_name +'.csv')
+					split_dfs = {split: load_csv(str(split +'.csv'), dir_path=asset_dir) for split in split_set}
+					label_df = load_csv(str(label_name +'.csv'), dir_path=asset_dir)
 
 					for access_level_name, access_level in access_levels.items():
 						labgroup_cols = get_subset(label_df.columns, access_level['label_group'])
-						access_dfs = [split_dfs[split] for split in access_level['data_access']]
-						access_df = reduce(inner_join, access_dfs)
+						access_df = reduce(inner_join, (split_dfs[split] for split in access_level['data_access']))
 
 						yield (split_group_name, asset, access_level_name, access_df, label_df[labgroup_cols])
 
