7e8975ba83a08e8e382e36ab3ef6e25b8b0a3034
Kevin <kpatelprime@gmail.com>
2018-03-07 00:21:25 -0800

    built out new util function to filter df rows using df.query(), without having to write numexpr directly using a search dictionary and some conventions

--- common_util.py
+++ common_util.py
@@ -1,24 +1,29 @@
-# Kevin Patel
+"""
+System level settings/constants and common utilities for all crunch subpackages.
+Kevin Patel
+"""
 
 import sys
 from os import sep, path, makedirs
 from os.path import dirname, basename, realpath, exists, isfile
 import pandas as pd
+from pandas.api.types import is_numeric_dtype, is_string_dtype, is_list_like
 from json import load
 from functools import partial
+from datetime import datetime
 from contextlib import contextmanager
 from timeit import default_timer
 
 
-# ********** SYSTEM SETTINGS **********
-# Project Root and Subpackage paths
+""" ********** SYSTEM SETTINGS ********** """
+"""Project Root and Subpackage paths"""
 CRUNCH_DIR = dirname(dirname(realpath(sys.argv[0]))) +sep
 RAW_DIR = CRUNCH_DIR +'raw' +sep
 DATA_DIR = CRUNCH_DIR +'data' +sep
 TRANSFORM_DIR = CRUNCH_DIR +'transform' +sep
 EDA_DIR = CRUNCH_DIR +'eda' +sep
 
-# Supported Pandas DF IO Formats
+"""Supported Pandas DF IO Formats"""
 FMT_EXTS = {
 	'csv': ('.csv',),
 	'feather': ('.feather',),
@@ -27,13 +32,16 @@ FMT_EXTS = {
 	'parquet': ('.parquet',)
 }
 
-# Default Pandas DF IO format
+"""Default Pandas DF IO format"""
 DF_DATA_FMT = 'parquet'
 
-# ********** CONSTANTS **********
+
+""" ********** GENERAL UTILS ********** """
+"""Constants"""
+EMPTY_STR=''
 
 
-# ********** FS AND GENERAL IO UTILS **********
+""" ********** FS AND GENERAL IO UTILS ********** """
 get_script_dir = lambda: dirname(realpath(sys.argv[0])) +sep
 get_parent_dir = lambda: dirname(dirname(realpath(sys.argv[0]))) +sep
 makedir_if_not_exists = lambda dir_path: makedirs(dir_path) if not exists(dir_path) else None
@@ -45,13 +53,15 @@ def load_json(fname, dir_path=None):
 		with open(fpath) as json_data:
 			return load(json_data)
 	else:
-		print(basename(fpath), 'must be in:', dirname(fpath))
-		sys.exit(2)
+		raise FileNotFoundError(str(basename(fpath) +'must be in:' +dirname(fpath)))
 
 
-# ********** PANDAS IO UTILS **********
+""" ********** PANDAS IO UTILS ********** """
 def load_df(fname, dir_path=None, subset=None, data_format=DF_DATA_FMT):
-	"""Assumes that source file has a non-default index column as the first column"""
+	"""
+	Read and return the df file in the given directory and
+	assume that the file has an index as the first column
+	"""
 	ext_tuple = FMT_EXTS[data_format]
 	fpath = str(dir_path + fname) if dir_path else fname
 	if (not fname.endswith(ext_tuple)):
@@ -73,8 +83,7 @@ def load_df(fname, dir_path=None, subset=None, data_format=DF_DATA_FMT):
 			print('error during load:', e)
 			sys.exit(2)
 	else:
-		print(basename(fpath), 'must be in:', dirname(fpath))
-		sys.exit(2)
+		raise FileNotFoundError(str(basename(fpath) +'must be in:' +dirname(fpath)))
 
 def dump_df(df, fname, dir_path=None, data_format=DF_DATA_FMT):
 	ext_tuple = FMT_EXTS[data_format]
@@ -95,18 +104,111 @@ def dump_df(df, fname, dir_path=None, data_format=DF_DATA_FMT):
 		sys.exit(2)
 
 
-# ********** PANDAS UTILS **********
+""" ********** PANDAS UTILS ********** """
 left_join = lambda a,b: a.join(b, how='left', sort=True)
 right_join = lambda a,b: a.join(b, how='right', sort=True)
 inner_join = lambda a,b: a.join(b, how='inner', sort=True)
 outer_join = lambda a,b: a.join(b, how='outer', sort=True)
 
 
-# ********** MISC UTILS **********
-# Selects a subset of str_list as dictated by qualifier_dict
-# returns the subset that satisfies:
-# IN(QUALIFIER_1 OR QUALIFIER_2 OR ... OR QUALIFIER_N-1) AND NOT IN(EXCLUDE_1 OR EXCLUDE_2 OR ... OR EXCLUDE_N-1)
+""" Pandas DF Row Search """
+"""Constants"""
+DEF_NUMEXPR = EMPTY_STR
+DEF_QUERY_JOIN = 'all'
+ALLOWED_TYPES = [int, float, str, tuple, list]
+
+def equals_numexpr(key, val):
+	"""Return numexpr expression for 'key equals value'"""
+	return str(key +'==' +val)
+
+def int_numexpr(key, val): equals_numexpr
+
+def float_numexpr(key, val): equals_numexpr
+
+def str_numexpr(key, val): equals_numexpr
+
+def list_numexpr(key, val): equals_numexpr
+
+def tuple_numexpr(key, val, tup_len):
+	"""
+	Use a string key and tuple value to create a numexpr
+	string representing an inequality expression and return it.
+	"""
+	if (tup_len == 2): 	# less than or greater than
+		return {
+			'lt': str(key +'<' + val[1]),
+			'lte': str(key +'<=' + val[1]),
+			'gt': str(key +'>' + val[1]),
+			'gte': str(key +'>=' + val[1])
+		}.get(val[0], DEF_NUMEXPR)
+
+	elif (tup_len == 3):	# in range or out of range
+		return {
+			'in': str(val[1] +'<' +key +'<' +val[2]),
+			'ine':str(val[1] +'<=' +key +'<=' +val[2]),
+			'out': str(key +'<' +val[1] +' or ' +key +'>' +val[2]),
+			'oute': str(key +'>=' +val[1] +' or ' +key +'<=' +val[2]),
+		}.get(val[0], DEF_NUMEXPR)
+
+def to_numexpr(key, val):
+	"""
+	Parse a key, val pair into a numexpr string and return it.
+	Dispatcher function for build_query.
+	# TODO - add assert typechecks of val at some point
+	"""
+	return {
+		int: int_numexpr(key, str(val)),
+		float: float_numexpr(key, str(val)),
+		str: str_numexpr(key, val),
+		list: list_numexpr(key, str(val)),
+		tuple: tuple_numexpr(key, tuple(map(str, val)), len(val))
+	}.get(type(val), DEF_NUMEXPR)
+
+def build_query(search_dict, join_method=DEF_QUERY_JOIN):
+	"""
+	Return a numexpr query defined by search_dict.
+	The join_method param sets how terms will be interpreted as a
+	set of row filters (AND, OR, None (returns unjoined list)).
+	The search_dict parameter has the following required format:
+		{
+			'$COL_NAME_1': TERM_1,
+			'$COL_NAME_2': TERM_2,
+			...
+		}
+	The supported filtering term types are:
+		- numeric: exact match
+		- string: exact match
+		- list(string...): subset match
+		- list(numeric...): subset match
+		- tuple(string, numeric) or tuple(string, numeric, numeric): range match
+	Currently no support for pandas series made of listlike objects. Currently
+		df.query has no way of facillitating the search of a listlike series.
+	The reason the format is defined thusly instead of using a
+	(more complex) nested dict structure is because:
+		1. Each df column (series) can only have one dtype anyway
+		2. Flat is better than nested
+		3. This format is good enough for most cases
+		4. More complicated queries composed using multiple build_query calls (use join_method=None)
+	"""
+	subqueries = [to_numexpr(col_name, search_term) for col_name, search_term in search_dict.items()]
+	return {
+		'all': ' and '.join(subqueries),
+		'any': ' or '.join(subqueries),
+		None: subqueries
+	}.get(join_method)
+
+def search_df(df, search_dict):
+	"""Top level function to search for rows which match search_dict"""
+	assert((key in df.columns) for key in search_dict.keys())
+	return df.query(build_query(search_dict)).index
+
+
+""" Pandas DF Column Filter  """
 def get_subset(str_list, qualifier_dict):
+	"""
+	Select a subset of str_list as dictated by qualifier_dict and return the subset, as list, that satisfies:
+	IN(QUALIFIER_1 OR QUALIFIER_2 OR ... OR QUALIFIER_N-1) AND NOT IN(EXCLUDE_1 OR EXCLUDE_2 OR ... OR EXCLUDE_N-1)
+	"""
 	selected = []
 
 	selected.extend([string for string in str_list if string in qualifier_dict['exact']])
@@ -120,7 +222,11 @@ def get_subset(str_list, qualifier_dict):
 	return list(dict.fromkeys(selected)) # Remove dups (casting to dict keys retains order in Python 3.6+) and cast to list
 
 
-# ********** PROFILING UTILS **********
+""" ********** MISC UTILS ********** """
+time_now = lambda: datetime.now().time()
+
+
+""" ********** PROFILING UTILS ********** """
 # The following class was written by stackoverflow's user bburns.km
 # https://stackoverflow.com/questions/7370801/measure-time-elapsed-in-python/41408510#41408510
 class benchmark(object):
