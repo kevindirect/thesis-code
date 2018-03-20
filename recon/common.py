70d851d0545e3249253f3b147ec5b4a83ff8d7e1
Kevin <kpatelprime@gmail.com>
2018-03-19 19:21:58 -0700

    added nancount functions to recon

--- recon/common.py
+++ recon/common.py
@@ -8,6 +8,18 @@ sys.path.insert(0, dirname(dirname(dirname(realpath(sys.argv[0])))))
 
 # ********** SPECIFIC TO THIS CRUNCH PACKAGE **********
 # RECON
+import pandas as pd
+
+# OTHER STAGE DEPENDENCIES
 
 
+# PACKAGE CONSTANTS
 
+
+# PACKAGE DEFAULTS
+dum = 0
+
+# PACKAGE UTIL FUNCTIONS
+count_nonnan = lambda df: len(df) - df.isnull().sum()
+count_nonzero = lambda df: df.apply(lambda ser: (ser.dropna(axis=0, how='any')!=0).sum())
+count_both = lambda df: pd.concat([count_nonnan(df), count_nonzero(df)], axis=1, names=['non_nan', 'non_zero'])
