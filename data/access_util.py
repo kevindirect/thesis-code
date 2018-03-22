0aaf01f88ef53e1564f06366ce8409e3ae6600a2
Kevin <kpatelprime@gmail.com>
2018-03-21 17:13:27 -0700

    added more snippets

--- data/access_util.py
+++ data/access_util.py
@@ -17,7 +17,7 @@ col_subsetters = load_json(col_subsetsfile, dir_path=ACCESS_UTIL_DIR)
 
 
 """
-	*********** ACCESS EXAMPLES ***********
+	*********** COLUMN FILTERING ***********
 	
 	Filter trmi etf news v2:
 		trmi_etf_news_v2_qual = [col_subsetters['#trmi']['all'], col_subsetters['#trmi']['etf_filter'], col_subsetters['#trmi']['news_filter'], col_subsetters['#trmi']['v2_filter']]
@@ -36,16 +36,30 @@ col_subsetters = load_json(col_subsetsfile, dir_path=ACCESS_UTIL_DIR)
 		vol_cols = chained_filter(df.columns, vol_ohlc_qual)
 		df.loc[:, pba_cols + vol_cols]
 
+
+	*********** ROW FILTERING ***********
+	
+	*********** TIME INDEX BASED ***********
+
 	Filter all rows before 2018:
 		date_range = {
 		    'id': ('lt', 2018)
 		}
 		df.loc[search_df(df, date_range)]
 
+	Filter rows to date range from 2001-02-03 to 2004-05-06
+		df.loc['2001-02-03':'2004-05-06']
+
 	Group by four year periods:
 		df.groupby(pd.Grouper(freq='4Y'))
 
+	*********** VALUE BASED ***********
 
+	Filter all col rows in range -.1 to .2 inclusive:
+		val_range = {
+		    'col': ('ine', -.1, .2)
+		}
+		df.loc[search_df(df, val_range)]
 
 
 """
