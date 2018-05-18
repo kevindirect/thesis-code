900a506285b8ea3a30ae564515701c0acb1b26ba
Kevin <kpatelprime@gmail.com>
2018-05-18 01:54:36 -0700

    scripts to pack and unpack project directory to and from a tarball gz

---
+++ pack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Kevin Patel
+
+echo "packing raw...";
+tar -czf raw.tar.gz raw;
+echo "packing data...";
+tar -czf data.tar.gz data;
+echo "packing mutate...";
+tar -czf mutate.tar.gz mutate;
+echo "packing recon...";
+tar -czf recon.tar.gz recon;
+echo "packing git...";
+tar -czf git.tar.gz .git;
+
+mkdir crunch;
+mv raw.tar.gz ./crunch/;
+mv data.tar.gz ./crunch/;
+mv mutate.tar.gz ./crunch/;
+mv recon.tar.gz ./crunch/;
+mv git.tar.gz ./crunch/;
+cp ./trmi.json ./crunch/;
+cp ./.gitignore ./crunch/;
+
+echo "packing whole...";
+tar -czf crunch.tar.gz crunch;
+mv crunch.tar.gz ..;
