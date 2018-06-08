2f47d82700ef0a9802d423a09891286596536281
kpatel28 <kpatelprime@gmail.com>
2018-06-08 00:44:41 -0700

    testing change to split_ser

--- recon/feat_util.py
+++ recon/feat_util.py
@@ -13,7 +13,9 @@ from recon.common import dum
 
 
 def split_ser(ser, num_cols, pfx=''):
+	split_df = pd.DataFrame(index=ser.index)
 	column_names = [str(pfx +'_' +str(i)) for i in range(num_cols)]
-	splitted = ser.str.split(',', num_cols, expand=True)
-	split_df = pd.DataFrame(data=splitted, index=ser.index, columns=column_names)
+	# splitted = ser.str.split(',', num_cols, expand=True)
+	split_df[column_names] = ser.str.split(',', num_cols, expand=True)
+	# split_df = pd.DataFrame(data=splitted, index=ser.index, columns=column_names)
 	return split_df
