cac4f4263614b928d0936f9c95532725ce62fcf9
kpatel28 <kpatelprime@gmail.com>
2018-06-08 16:30:09 -0700

    testing version with lower code complexity

--- recon/exp_test.py
+++ recon/exp_test.py
@@ -8,17 +8,19 @@ import logging
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
 
 from common_util import get_custom_biz_freq, get_subset, inner_join, count_nn_df, remove_dups_list, list_get_dict, list_set_dict, benchmark
 from data.data_api import DataAPI
 from data.access_util import df_getters as dg, col_subsetters2 as cs2
 from recon.common import dum
-from recon.feat_util import split_ser, handle_nans_df
+from recon.feat_util import split_cluster_ser
 from recon.label_util import get_base_labels, default_fct, fastbreak_fct, confidence_fct, fastbreak_confidence_fct
 
 def test(argv):
 	logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+	km_info = {'cl': KMeans(n_clusters=4, random_state=0), 'sfx': 'kmeans(4)'}
 	features_paths, features = DataAPI.load_from_dg(dg['sax']['dzn_sax'], cs2['sax']['dzn_sax'], subset=['raw_pba', 'raw_vol'])
 	logging.info('loaded features')
 
@@ -41,46 +43,65 @@ def test(argv):
 				for base_label in get_base_labels(label_df.columns[1:])}
 
 			# Iterate through all variations of this label
-			for base_label, base_label_sel in label_col_sel.items():
+			for base_label, base_label_cols in label_col_sel.items():
 				logging.debug('base label: ' +base_label)
 				dir_col_name = '_'.join([base_label, 'dir'])
-				fct_df = default_fct(label_df[base_label_sel], name_pfx=base_label)
-				label_fct_df[dir_col_name] = fct_df[dir_col_name]
+				fct_df = default_fct(label_df[base_label_cols], name_pfx=base_label)
+				label_fct_df = fct_df[[dir_col_name]]
 
 			label_fct_df.index = label_fct_df.index.normalize()
-			# print(label_fct_df)
-			# return
+		logging.info('done transforming labels')
 
-			for label_fct_col in label_fct_df:
-				label_fct_shf_df = label_fct_df[[label_fct_col]].dropna().astype(int)
-				shift_freq = get_custom_biz_freq(label_fct_shf_df)
-				label_fct_shf_df = label_fct_shf_df.shift(periods=-1, freq=None, axis=0).dropna().astype(int)
+		for feature_path in filter(lambda fpath: fpath[0]==asset, features_paths):
+			cl_transformer = partial(split_cluster_ser, sklearn_cluster=km_info['cl'], col_name_pfx='_'.join(feature_path[1:]), cluster_sfx=km_info['sfx'])
+			feat_df = list_get_dict(features, feature_path).transform(cl_transformer)
+			print(feat_df)
+		logging.info('done transforming features')
+			# for col_name in feat_df:
+			# 	col_name_prefix = '_'.join(feature_path[1:] +[col_name])
+			# 	logging.info(col_name_prefix)
+			# 	sax_df = handle_nans_df(split_ser(feat_df[col_name], 8, pfx=col_name_prefix))
+			# 	temp_df = inner_join(label_fct_shf_df, sax_df)
+			# 	feats_only = temp_df[temp_df.columns[1:]]
+			# 	# handled_df[sax_df.columns] = sax_df
+			# 	# print('num_rows:', count_nn_df(sax_df).iloc[0])
 
-				# Iterate through all feature sets
-				for feature_path in filter(lambda fpath: fpath[0]==asset, features_paths):
-					feat_df = list_get_dict(features, feature_path)
-					np_feat = {}
-					handled_df = pd.DataFrame(index=feat_df.index)
-					handled_df[label_fct_col] = label_fct_shf_df[label_fct_col]
+			# 	kmeans = KMeans(n_clusters=4, random_state=0).fit(feats_only.values)
+			# 	handled_df[col_name_prefix +'_' +'kmeans(4)'] = kmeans.labels_
+			# 	pd.DataFrame(data=labels, columns=['cluster'], index=collapsed.index)
 
-					for col_name in feat_df:
-						col_name_prefix = '_'.join(feature_path[1:] +[col_name])
-						logging.info(col_name_prefix)
-						sax_df = handle_nans_df(split_ser(feat_df[col_name], 8, pfx=col_name_prefix))
-						temp_df = inner_join(label_fct_shf_df, sax_df)
-						feats_only = temp_df[temp_df.columns[1:]]
-						# handled_df[sax_df.columns] = sax_df
-						# print('num_rows:', count_nn_df(sax_df).iloc[0])
 
-						kmeans = KMeans(n_clusters=4, random_state=0).fit(feats_only.values)
-						handled_df[col_name_prefix +'_' +'kmeans(4)'] = kmeans.labels_
-				print(handled_df)
+			# for label_fct_col in label_fct_df:
+			# 	label_fct_shf_df = label_fct_df[[label_fct_col]].dropna()
+			# 	shift_freq = get_custom_biz_freq(label_fct_shf_df)
+			# 	label_fct_shf_df = label_fct_shf_df.shift(periods=-1, freq=None, axis=0).dropna()
 
-				print(np_feat)
+			# 	# Iterate through all feature sets
+			# 	for feature_path in filter(lambda fpath: fpath[0]==asset, features_paths):
+			# 		feat_df = list_get_dict(features, feature_path)
+			# 		np_feat = {}
+			# 		handled_df = pd.DataFrame(index=feat_df.index)
+			# 		handled_df[label_fct_col] = label_fct_shf_df[label_fct_col]
+
+			# 		for col_name in feat_df:
+			# 			col_name_prefix = '_'.join(feature_path[1:] +[col_name])
+			# 			logging.info(col_name_prefix)
+			# 			sax_df = handle_nans_df(split_ser(feat_df[col_name], 8, pfx=col_name_prefix))
+			# 			temp_df = inner_join(label_fct_shf_df, sax_df)
+			# 			feats_only = temp_df[temp_df.columns[1:]]
+			# 			# handled_df[sax_df.columns] = sax_df
+			# 			# print('num_rows:', count_nn_df(sax_df).iloc[0])
+
+			# 			kmeans = KMeans(n_clusters=4, random_state=0).fit(feats_only.values)
+			# 			handled_df[col_name_prefix +'_' +'kmeans(4)'] = kmeans.labels_
+			# 			pd.DataFrame(data=labels, columns=['cluster'], index=collapsed.index)
+			# 	print(handled_df)
+
+			# 	print(np_feat)
 
 
 def test_logistic_reg(lab_feat_df):
-	lab = la
+	pass
 
 def make_sw_dict(sw_str):
 	return {
--- recon/feat_util.py
+++ recon/feat_util.py
@@ -14,7 +14,7 @@ from recon.common import dum
 
 def split_ser(ser, num_cols, pfx=''):
 	split_df = pd.DataFrame(index=ser.index)
-	column_names = [str(pfx +'_' +str(i)) for i in range(num_cols)]
+	column_names = ['_'.join([pfx, str(i)]) for i in range(num_cols)]
 	split_df[column_names] = ser.str.split(',', num_cols, expand=True)
 	return split_df
 
@@ -22,3 +22,15 @@ def handle_nans_df(df, method='drop'):
 	# TODO - add in threshold
 	# TODO - add in a method to ffill if under threshold
 	return df.dropna(axis=0, how='any')
+
+def split_cluster_ser(ser, sklearn_cluster, col_name_pfx='', cluster_sfx=''):
+	col_pfx = '_'.join([col_name_pfx, ser.name])
+	logging.info(col_pfx)
+
+	sax_df = handle_nans_df(split_ser(ser, 8, pfx=col_pfx))
+	clustered_values = sklearn_cluster.fit(sax_df.values).labels_
+	clustered = pd.Series(data=clustered_values, name='_'.join(col_pfx, cluster_sfx), index=sax_df.index)
+
+	return clustered
+	# temp_df = inner_join(label_fct_shf_df, sax_df)
+	# feats_only = temp_df[temp_df.columns[1:]]
