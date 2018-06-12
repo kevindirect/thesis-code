5dc4d026377b27158549792af38312ada112f26b
kpatel28 <kpatelprime@gmail.com>
2018-06-11 18:40:43 -0700

    making experiment test loop

--- recon/exp_test.py
+++ recon/exp_test.py
@@ -21,7 +21,7 @@ def test(argv):
 	logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 	km_info = {'cl': KMeans(n_clusters=8, random_state=0), 'sfx': 'kmeans(8)'}
-	features_paths, features = DataAPI.load_from_dg(dg['sax']['dzn_sax'], cs2['sax']['dzn_sax'], subset=['raw_pba', 'raw_vol', 'thresh'])
+	features_paths, features = DataAPI.load_from_dg(dg['sax']['dzn_sax'], cs2['sax']['dzn_sax'], subset=['raw_pba', 'raw_vol'])
 	logging.info('loaded features')
 
 	labels_paths, labels = DataAPI.load_from_dg(dg['labels']['itb'], cs2['labels']['itb'])
@@ -33,11 +33,11 @@ def test(argv):
 	for asset in assets:
 		logging.info('asset: ' +str(asset))
 
-		# for lab_df in gen_label_dfs(labels, labels_paths, asset):
-		# 	print(count_nn_df(lab_df))
+		for lab_df in gen_label_dfs(labels, labels_paths, asset):
+			print(count_nn_df(lab_df))
 
-		for feat_df in gen_cluster_feats(features, features_paths, asset, km_info):
-			print(count_nn_df(feat_df))
+			for feat_df in gen_cluster_feats(features, features_paths, asset, km_info):
+				print(count_nn_df(feat_df))
 
 
 			# for label_fct_col in label_fct_df:
