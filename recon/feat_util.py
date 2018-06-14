f68242bcb64bac285be11338e076b7ca8fbdd825
kpatel28 <kpatelprime@gmail.com>
2018-06-14 03:07:44 -0700

    single feat df splitting (for use with kmeans pipeline)

--- recon/feat_util.py
+++ recon/feat_util.py
@@ -7,7 +7,7 @@ import logging
 import numpy as np
 import pandas as pd
 
-from common_util import remove_dups_list, list_get_dict
+from common_util import remove_dups_list, list_get_dict, get_nmost_nulled_cols_df
 from recon.common import dum
 
 
@@ -17,10 +17,14 @@ def split_ser(ser, num_cols, pfx=''):
 	split_df[column_names] = ser.str.split(',', num_cols, expand=True)
 	return split_df
 
-def handle_nans_df(df, method='drop'):
+def handle_nans_df(df, method='drop_row', max_col_drop=1):
 	# TODO - add in threshold
 	# TODO - add in a method to ffill if under threshold
-	return df.dropna(axis=0, how='any')
+	if (method == 'drop_row'):
+		return df.dropna(axis=0, how='any')
+	elif (method == 'drop_col'):
+		drop_cols = get_nmost_nulled_cols_df(df, n=max_col_drop)
+		return df.drop(labels=drop_cols, axis=1)
 
 def split_cluster_ser(ser, sklearn_cluster, col_pfx=None):
 	col_pfx = ser.name if (col_pfx is None) else col_pfx
@@ -39,3 +43,12 @@ def gen_cluster_feats(feat_dict, feat_paths, asset_name, cluster_info):
 			.add_prefix(cname_pfx)
 
 		yield feat_df
+
+def gen_split_feats(feat_dict, feat_paths, asset_name, num_cols=8):
+	for feat_path in filter(lambda feat_path: feat_path[0]==asset_name, feat_paths):
+		cname_pfx = '_'.join(feat_path[:0:-1] + [cluster_info['sfx']]) + '_'
+		logging.info(cname_pfx)
+
+		feat_df = list_get_dict(feat_dict, feat_path):
+		for feat_name in feat_df:
+			yield handle_nans_df(split_ser(feat_df[feat_name], num_cols, pfx=feat_name)).add_prefix(cname_pfx)
