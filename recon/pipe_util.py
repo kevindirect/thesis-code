679d91bf53ffa60ea162f2feb1aea88b52fba81b
kpatel28 <kpatelprime@gmail.com>
2018-06-14 03:44:37 -0700

    not a named parameter?

--- recon/pipe_util.py
+++ recon/pipe_util.py
@@ -32,6 +32,6 @@ def extract_pipeline(dictionary):
 	Converts a passed pipeline dictionary into a sklearn Pipeline object and parameter grid
 	"""
 	pipeline_steps = [(step_name, translate_step(step_name)) for step_name in dictionary['steps']]
-	pipeline = make_pipeline(steps=pipeline_steps)
+	pipeline = make_pipeline(pipeline_steps)
 	
 	return pipeline, extract_pipeline['grid']
