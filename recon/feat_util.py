b71e7db4747cf83408cc420de489aeba0baf8374
kpatel28 <kpatelprime@gmail.com>
2018-06-14 19:31:52 -0700

    debugging

--- recon/feat_util.py
+++ recon/feat_util.py
@@ -15,7 +15,7 @@ from recon.common import dum
 def split_ser(ser, pfx=''):
 	# split_df = pd.DataFrame(index=ser.index)
 	list_ser = ser.str.split(',', n=-1, expand=False)
-	split_df = pd.DataFrame.from_records(zip_longest(*list_ser.values.T))
+	split_df = pd.DataFrame.from_records(zip_longest(*list_ser.values)).T
 	print(split_df)
 	split_df.add_prefix(pfx +'_')
 	# column_names = ['_'.join([pfx, str(i)]) for i in range(len(split_df.columns))]
