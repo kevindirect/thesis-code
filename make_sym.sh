2107f4b9cd1c080c51b36bac6f5ff6accfe40105
kpatel28 <kpatelprime@gmail.com>
2018-07-11 14:44:52 -0700

    less symbol groups

--- make_sym.sh
+++ make_sym.sh
@@ -1,8 +1,4 @@
-python3 -m mutate.symbolize -a 2 -m 7;
-python3 -m mutate.symbolize -a 3 -m 7;
-python3 -m mutate.symbolize -a 5 -m 7;
-python3 -m mutate.symbolize -a 9 -m 7;
-python3 -m mutate.symbolize -a 2 -m 8;
-python3 -m mutate.symbolize -a 3 -m 8;
-python3 -m mutate.symbolize -a 5 -m 8;
-python3 -m mutate.symbolize -a 9 -m 8;
+python3 -m mutate.symbolize -a 2;
+python3 -m mutate.symbolize -a 3;
+python3 -m mutate.symbolize -a 4;
+python3 -m mutate.symbolize -a 8;
