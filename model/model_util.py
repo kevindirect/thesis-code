b9817534b9fdcdbcc80a2693278be52cf4128644
kpatel28 <kpatelprime@gmail.com>
2018-08-22 05:08:17 -0700

    removed unneeded imports

--- model/model_util.py
+++ model/model_util.py
@@ -11,12 +11,13 @@ import pandas as pd
 from common_util import MODEL_DIR, DT_HOURLY_FREQ, DT_CAL_DAILY_FREQ, load_json, dump_json, get_cmd_args, best_match, remove_dups_list, list_get_dict, is_empty_df, search_df, benchmark
 from data.data_api import DataAPI
 from data.access_util import df_getters as dg, col_subsetters2 as cs2
-from mutate.common import default_runt_dir_name, default_trfs_dir_name
-from mutate.runt_util import RUNT_FN_TRANSLATOR, RUNT_TYPE_TRANSLATOR, RUNT_FREQ_TRANSLATOR
+from model.common import dum
 
 
 def main(argv):
 	
+
+
 def model_test():
 	
 	
