ce63a0e25102e624d0678378dfdc2d26187dbd2b
kpatel28 <kpatelprime@gmail.com>
2018-09-26 14:01:09 -0700

    moved PurgedKFold to hyperfit module

--- recon/hyperfit.py
+++ recon/hyperfit.py
@@ -8,14 +8,47 @@ import numpy as np
 import pandas as pd
 
 from dask_ml.model_selection import GridSearchCV
+from sklearn.cross_validation import _BaseKFold
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, log_loss, f1_score, fbeta_score, precision_score, recall_score
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, classification_report, roc_curve, precision_recall_curve
 
 from common_util import remove_dups_list, list_get_dict
 from recon.common import dum
-from recon.model_util import PurgedKFold
 
+class PurgedKFold(_BaseKFold):
+	"""
+	Extend KFold class to work with labels that span intervals
+	The train is purged of observations overlapping test-label intervals
+	Test set is assumed contiguous (shuffle=False), w/o training samples in between
+
+	Lopez De Prado, Advances in Financial Machine Learning (p. 131)
+	"""
+	def __init__(self, n_splits=3, t1=None, pct_embargo=0.):
+		if (not isinstance(t1, pd.Series)):
+			raise ValueError('Label Through Dates must be a pd.Series')
+
+		super(PurgedKFold, self).__init_(n_splits, shuffle=False, random_state=None)
+		self.t1 = t1
+		self.pct_embargo = pct_embargo
+
+	def split (self, X, y=None, groups=None):
+		if ((X.index == self.t1.index).sum() != len(self.t1)):
+			raise ValueError('X and Through Dates must have the same index')
+
+		indices = np.arange(X.shape[0])
+		embargo = int(X.shape[0]*self.pct_embargo)
+		test_starts = [(i[0], i[-1]+1) for i in np.array_split(np.arange(X.shape[0]), self.n_splits)]
+
+		for i, j in test_starts:
+			t0 = self.t1.index[i] # start of test set
+			test_indices = indices[i:j]
+			max_t1_idx = self.t1.index.searchsorted(self.t1[test_indices].max())
+			train_indices = self.t1.index.searchsorted(self.t1[self.t1 <= t0].index)
+			if (max_t1_idx < X.shape[0]): # right train (with embargo)
+				train_indices = np.concatenate((train_indices, indices[max_t1_idx+embargo:]))
+
+			yield train_indices, test_indices
 
 class MyPipeline(Pipeline):
 	"""
--- recon/model_util.py
+++ recon/model_util.py
@@ -7,7 +7,6 @@ import logging
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split, KFold, TimeSeriesSplit
-from sklearn.cross_validation import _BaseKFold
 
 from common_util import RECON_DIR
 from recon.common import dum
@@ -23,7 +22,6 @@ def get_train_test_split(feats, lab, train_ratio=.8, to_np=True):
 		return train_test_split(feats.values, lab.values, train_size=train_ratio, shuffle=False)
 	else:
 		return train_test_split(feats, lab, train_size=train_ratio, shuffle=False)
-
 
 def gen_time_series_split(feats, lab, num_splits=5, max_train=None):
 	tscv = TimeSeriesSplit(n_splits=num_splits, max_train_size=max_train)
@@ -57,38 +55,3 @@ def extract_cv_splitter(dictionary):
 	cv_splitter = translate_cv(dictionary['name'], cv_params=dictionary['params'])
 	
 	return cv_splitter
-
-class PurgedKFold(_BaseKFold):
-	"""
-	Extend KFold class to work with labels that span intervals
-	The train is purged of observations overlapping test-label intervals
-	Test set is assumed contiguous (shuffle=False), w/o training samples in between
-
-	Lopez De Prado, Advances in Financial Machine Learning (p. 131)
-	"""
-	def __init__(self, n_splits=3, t1=None, pct_embargo=0.):
-		if (not isinstance(t1, pd.Series)):
-			raise ValueError('Label Through Dates must be a pd.Series')
-
-		super(PurgedKFold, self).__init_(n_splits, shuffle=False, random_state=None)
-		self.t1 = t1
-		self.pct_embargo = pct_embargo
-
-	def split (self, X, y=None, groups=None):
-		if ((X.index == self.t1.index).sum() != len(self.t1)):
-			raise ValueError('X and Through Dates must have the same index')
-
-		indices = np.arange(X.shape[0])
-		embargo = int(X.shape[0]*self.pct_embargo)
-		test_starts = [(i[0], i[-1]+1) for i in np.array_split(np.arange(X.shape[0]), self.n_splits)]
-
-		for i, j in test_starts:
-			t0 = self.t1.index[i] # start of test set
-			test_indices = indices[i:j]
-			max_t1_idx = self.t1.index.searchsorted(self.t1[test_indices].max())
-			train_indices = self.t1.index.searchsorted(self.t1[self.t1 <= t0].index)
-			if (max_t1_idx < X.shape[0]): # right train (with embargo)
-				train_indices = np.concatenate((train_indices, indices[max_t1_idx+embargo:]))
-
-			yield train_indices, test_indices
-
