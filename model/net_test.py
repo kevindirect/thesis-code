afedb31719ecd7c3f55409f827cd7dbe5b8065a6
kpatel28 <kpatelprime@gmail.com>
2018-10-09 07:13:28 -0700

    added import

--- model/net_test.py
+++ model/net_test.py
@@ -13,7 +13,7 @@ from dask import delayed, compute, visualize
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD, RMSprop, Adadelta, Adam, Adamax, Nadam
-from common_util import RECON_DIR, JSON_SFX_LEN, DT_CAL_DAILY_FREQ, get_cmd_args, reindex_on_time_mask, gb_transpose, pd_common_index_rows, filter_cols_below, dump_df, load_json, outer_join, list_get_dict, chained_filter, benchmark
+from common_util import RECON_DIR, JSON_SFX_LEN, DT_CAL_DAILY_FREQ, get_cmd_args, in_debug_mode, reindex_on_time_mask, gb_transpose, pd_common_index_rows, filter_cols_below, dump_df, load_json, outer_join, list_get_dict, chained_filter, benchmark
 from model.common import DATASET_DIR, FILTERSET_DIR, EXPECTED_NUM_HOURS, default_dataset, default_filterset, default_nt_filter, default_target_col_idx
 from recon.dataset_util import prep_dataset, prep_labels, gen_group
 from recon.model_util import get_train_test_split, gen_time_series_split
