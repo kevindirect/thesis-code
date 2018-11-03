6b2ec5d1b878835586f403e1af644052f7f5f449
kpatel28 <kpatelprime@gmail.com>
2018-11-02 23:27:40 -0700

    fix shape parameter

--- model/models/BinaryClassifierExperiment.py
+++ model/models/BinaryClassifierExperiment.py
@@ -38,7 +38,7 @@ class BinaryClassifierExperiment(ClassifierExperiment):
 			Standard classifier objective function to minimize.
 			"""
 			try:
-				compiled = self.make_model(params, features.shape[1])
+				compiled = self.make_model(params, (features.shape[1],))
 
 				if (retain_holdout):
 					results = self.fit_model(params, compiled, feat_train, lab_train, val_split=test_ratio, shuffle=shuffle)
--- model/models/ClassifierExperiment.py
+++ model/models/ClassifierExperiment.py
@@ -105,7 +105,7 @@ class ClassifierExperiment:
 			Standard classifier objective function to minimize.
 			"""
 			try:
-				compiled = self.make_model(params, features.shape[1])
+				compiled = self.make_model(params, (features.shape[1],))
 
 				if (retain_holdout):
 					results = self.fit_model(params, compiled, feat_train, lab_train, val_split=test_ratio, shuffle=shuffle)
