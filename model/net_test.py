6900d501bbe3fe076ce7bc6084ac64bf9ce36944
kpatel28 <kpatelprime@gmail.com>
2018-11-02 23:17:53 -0700

    debug

--- model/net_test.py
+++ model/net_test.py
@@ -130,7 +130,7 @@ def test_model(model_exp, params, feats, label, test_ratio=.25, shuffle=False):
 	# exp = delayed(model_exp)()
 	# mod = delayed(exp.make_model)(params, feats.shape[1])
 	# fit = delayed(exp.fit_model)(params, mod, feat_train, lab_train, feat_val=feat_test, lab_val=lab_test, val_split=test_ratio, shuffle=shuffle)
-	feat_train, feat_test, lab_train, lab_test = get_train_test_split(feats, label, test_ratio=test_ratio, shuffle=shuffle)
+	feat_train, feat_test, lab_train, lab_test = delayed(get_train_test_split, nout=4)(feats, label, test_ratio=test_ratio, shuffle=shuffle).compute()
 	exp = model_exp()
 	mod = exp.make_model(params, feats.shape[1])
 	fit = exp.fit_model(params, mod, feat_train, lab_train, feat_val=feat_test, lab_val=lab_test, val_split=test_ratio, shuffle=shuffle)
