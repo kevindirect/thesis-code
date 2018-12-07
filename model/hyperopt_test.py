d650c567414f4ce9731040c09b87d84432e7fae7
kpatel28 <kpatelprime@gmail.com>
2018-12-07 13:35:28 -0800

    added model commandline arg

--- model/hyperopt_test.py
+++ model/hyperopt_test.py
@@ -1,37 +1,32 @@
 """
 Kevin Patel
 """
-
 import sys
 import os
-from os import sep
-from os.path import splitext
-from itertools import product
-from functools import partial, reduce
 import logging
 
 import numpy as np
 import pandas as pd
-from dask import delayed, compute, visualize
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from hyperopt import fmin, tpe, Trials
 
-from common_util import MODEL_DIR, RECON_DIR, JSON_SFX_LEN, str_to_list, get_cmd_args, pd_common_index_rows, load_json, benchmark
-from model.common import DATASET_DIR, FILTERSET_DIR, default_dataset, default_opt_filter
-from model.model_util import datagen, prepare_transpose_data, prepare_label_data
-from model.model.ThreeLayerBinaryFFN import ThreeLayerBinaryFFN
-from model.model.OneLayerBinaryLSTM import OneLayerBinaryLSTM
+from common_util import JSON_SFX_LEN, str_to_list, get_cmd_args, pd_common_index_rows, load_json, benchmark
+from model.common import DATASET_DIR, FILTERSET_DIR, default_model, default_dataset, default_opt_filter
+from model.model_util import BINARY_CLF_MAP, datagen, prepare_transpose_data, prepare_label_data
 from recon.dataset_util import prep_dataset
-from recon.split_util import get_train_test_split, pd_binary_clip
+from recon.split_util import pd_binary_clip
 
 
 def hyperopt_test(argv):
-	cmd_arg_list = ['dataset=', 'filterset=', 'idxfilters=', 'assets=']
+	cmd_arg_list = ['model=', 'dataset=', 'filterset=', 'idxfilters=', 'assets=']
 	cmd_input = get_cmd_args(argv, cmd_arg_list, script_name='hyperopt_test')
+	mod_code = cmd_input['model='] if (cmd_input['model='] is not None) else default_model
 	dataset_name = cmd_input['dataset='] if (cmd_input['dataset='] is not None) else default_dataset
 	filterset_name = cmd_input['filterset='] if (cmd_input['filterset='] is not None) else '_'.join(['default', dataset_name])
 	filter_idxs = str_to_list(cmd_input['idxfilters=']) if (cmd_input['idxfilters='] is not None) else default_opt_filter
 	assets = str_to_list(cmd_input['assets=']) if (cmd_input['assets='] is not None) else None
 
+	mod = BINARY_CLF_MAP[mod_code]()
+	mod_name = get_class_name(mod)
 	dataset_dict = load_json(dataset_name, dir_path=DATASET_DIR)
 	filter_dict = load_json(filterset_name, dir_path=FILTERSET_DIR)
 
@@ -41,21 +36,22 @@ def hyperopt_test(argv):
 		filterset.extend(selected)
 	dataset = prep_dataset(dataset_dict, assets=assets, filters_map={'features': filterset})
 
+	logging.info('model: {}'.format(mod_name))
 	logging.info('assets: {}'.format(str('all' if (assets==None) else ', '.join(assets))))
 	logging.info('dataset: {} {} df(s)'.format(len(dataset['features']), dataset_name[:-JSON_SFX_LEN]))
 	logging.info('filter: {} [{}]'.format(filterset_name[:-JSON_SFX_LEN], str(', '.join(filter_idxs))))
 	logging.debug('filterset: {}'.format(filterset))
 
 	logging.info('executing...')
-	for feature, label in datagen(dataset, feat_prep_fn=prepare_transpose_data, label_prep_fn=prepare_label_data, how='ser_to_ser'):
+	for fpath, lpath, frec, lrec, fcol, lcol, feature, label in datagen(dataset, feat_prep_fn=prepare_transpose_data, label_prep_fn=prepare_label_data, how='ser_to_ser'):
 		pos_label, neg_label = pd_binary_clip(label)
 		f, lpos, lneg = pd_common_index_rows(feature, pos_label, neg_label)
 
 		logging.info('pos dir model experiment')
-		run_trials(ThreeLayerBinaryFFN, f, lpos)
+		run_trials(mod, f, lpos)
 
 		logging.info('neg dir model experiment')
-		run_trials(ThreeLayerBinaryFFN, f, lneg)
+		run_trials(mod, f, lneg)
 
 def run_trials(model_exp, features, label):
 	exp = model_exp()
