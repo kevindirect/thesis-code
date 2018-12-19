56d5dcc232216103c6df893c446305ffe293a61a
kpatel28 <kpatelprime@gmail.com>
2018-12-19 00:35:00 -0800

    test

--- model/model_util.py
+++ model/model_util.py
@@ -10,11 +10,11 @@ import pandas as pd
 
 from common_util import identity_fn
 from model.common import EXPECTED_NUM_HOURS
-from model.one_bin_cnn import OneLayerBinaryCNN
-from model.one_bin_gru import OneLayerBinaryGRU
-from model.one_bin_lcl import OneLayerBinaryLCL
-from model.one_bin_lstm import OneLayerBinaryLSTM
-from model.three_bin_ffn import ThreeLayerBinaryFFN
+from .model.one_bin_cnn import OneLayerBinaryCNN
+from .model.one_bin_gru import OneLayerBinaryGRU
+from .model.one_bin_lcl import OneLayerBinaryLCL
+from .model.one_bin_lstm import OneLayerBinaryLSTM
+from .model.three_bin_ffn import ThreeLayerBinaryFFN
 
 
 """ ********** MODELS ********** """
