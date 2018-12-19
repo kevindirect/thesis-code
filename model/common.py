6c4536eb58558c565f2d094043f875d3401ec800
kpatel28 <kpatelprime@gmail.com>
2018-12-18 21:45:00 -0800

    remove exp dir entry

--- model/common.py
+++ model/common.py
@@ -20,7 +20,6 @@ from common_util import RECON_DIR, MODEL_DIR
 DATASET_DIR = RECON_DIR +'dataset' +sep
 FILTERSET_DIR = RECON_DIR +'filterset' +sep
 MODELS_DIR = MODEL_DIR +'model' +sep
-EXP_DIR = MODEL_DIR +'exp' +sep
 EXPECTED_NUM_HOURS = 8
 TRIALS_COUNT = 100
 TEST_RATIO = .2
