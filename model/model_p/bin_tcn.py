f7b70d6c1856f885fdc27f4771f1945014c456f3
kpatel28 <kpatelprime@gmail.com>
2019-01-25 16:28:30 -0800

    testing change

--- model/model_p/bin_tcn.py
+++ model/model_p/bin_tcn.py
@@ -32,7 +32,7 @@ class BinaryTCN(TemporalMixin, BinaryClassifier):
 	"""
 	def __init__(self, other_space={}):
 		default_space = {
-			'input_windows': hp.choice('input_windows', [4]),
+			'input_windows': hp.choice('input_windows', [3]),
 			'topology': hp.choice('topology', [[3, 1]]),
 			'kernel_size': hp.choice('kernel_size', [4]),
 			'stride': hp.choice('stride', [1]),
