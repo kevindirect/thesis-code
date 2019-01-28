46ede15110456cdee5cdb089616d785b4221464e
kpatel28 <kpatelprime@gmail.com>
2019-01-28 03:50:57 -0800

    debug shape

--- model/model_p/tcn_util.py
+++ model/model_p/tcn_util.py
@@ -173,5 +173,7 @@ class TCN_Classifier(nn.Module):
 		out_embedding = self.tcn(x)
 		# out = self.linear(out_embedding[:, :, -1])
 		# out = self.linear(out_embedding).double()
+		print('BEFORE: ', out_embedding.shape)
+		print('AFTER:  ', out_embedding[:, :, -1].shape)
 		out = self.linear(out_embedding[:, :, -1])
 		return self.output(out)
