29db5783aeb322031f6aecba4e8b245375286d68
kpatel28 <kpatelprime@gmail.com>
2019-02-22 02:40:47 -0800

    debug

--- model/model_p/pytorch_model.py
+++ model/model_p/pytorch_model.py
@@ -140,11 +140,9 @@ class Model:
 		prediction_batch = torch.argmax(outputs_batch, dim=1) # Convert network outputs into predictions
 		print(lab_batch.shape, lab_batch)
 		print(prediction_batch.shape, prediction_batch)
-		
-		print('acc:', accuracy_score(lab_batch, prediction_batch))
-		print('f1:', f1_score(lab_batch, prediction_batch))
 		for name, fn in self.metrics_fns:
-			print(name, float(fn(lab_batch, prediction_batch)))
+			print(name)
+			print(float(fn(lab_batch, prediction_batch)))
 		# metrics = {name: float(fn(lab_batch, prediction_batch)) for name, fn in self.metrics_fns}
 
 		if (optimizer is not None):
