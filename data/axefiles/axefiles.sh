abcb56e25eca001f6ec9713f043234a785159e41
kpatel28 <kpatelprime@gmail.com>
2019-06-13 15:19:32 -0700

    shell scripts I used for axefile modifications

---
+++ data/axefiles/axefiles.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# List all axefiles with symlinks excluded.
+find */*.json -type f;
--- data/axefiles/jsoncommafix.sh
+++ data/axefiles/jsoncommafix.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sed -i --follow-symlinks '/^[[:space:]]*$/d' "$@";
-sed -i --follow-symlinks -E -n 'H; x; s:,(\s*\n\s*}):\1:; P; ${x; p}' "$@";
-sed -i --follow-symlinks '/^[[:space:]]*$/d' "$@";
+# Fixes trailing commas in json files
+sed -i --follow-symlinks '/^[[:space:]]*$/d' "$@";				# Remove empty lines
+sed -i --follow-symlinks -E -n 'H; x; s:,(\s*\n\s*}):\1:; P; ${x; p}' "$@";	# Remove trailing comma
+sed -i --follow-symlinks '/^[[:space:]]*$/d' "$@";				# Remove empty lines
