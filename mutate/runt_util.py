9b3e74d9ee2e8a32ee0bab4ca4255b111519f973
kpatel28 <kpatelprime@gmail.com>
2019-06-22 03:26:16 -0700

    reorganizing, cleanup, refactoring

--- mutate/runt_util.py
+++ mutate/runt_util.py
@@ -18,52 +18,32 @@ import pandas as pd
 from common_util import DT_HOURLY_FREQ, DT_BIZ_DAILY_FREQ, DT_CAL_DAILY_FREQ, compose, null_fn, identity_fn, get_custom_biz_freq, window_iter, col_iter, all_equal, is_real_num
 from common_util import ser_range_center_clip, pd_slot_shift, concat_map, substr_ad_map, all_equal, first_element, first_letter_concat, arr_nonzero, apply_nz_nn, one_minus
 from mutate.common import STANDARD_DAY_LEN
-from mutate.tfactory_util import *
-
-
-""" ********** APPLY FUNCTIONS ********** """
-def apply_rt_df(df, var, freq, ser_transform_fn_str, name_map_fn_str, dna=True):	# regular transform
-	res = df.transform(ser_transform_fn)
-	return res.dropna(axis=0, how='all') if (dna) else res
-
-def apply_gbt_df(df, ser_transform_fn, agg_freq, name_map_fn, dna=True):	# groupby transform
-	res = df.groupby(pd.Grouper(freq=agg_freq)).transform(ser_transform_fn)
-	return res.dropna(axis=0, how='all') if (dna) else res
-
-def apply_gagg_df(df, ser_agg_fn, agg_freq, name_map_fn, dna=True):		# groupby aggregation
-	res = df.groupby(pd.Grouper(freq=agg_freq)).agg(ser_agg_fn)
-	return res.dropna(axis=0, how='all') if (dna) else res
-
-def apply_btw_df(df, binary_apply_fn, freq, name_map_fn, dna=True):	# binary transform sliding window
-	res = pd.DataFrame(index=df.index)
-	for col_a, col_b in window_iter(df.columns):
-		res.loc[:, name_map_fn(col_a, col_b)] = binary_apply_fn(df[col_a], df[col_b])
-	return res.dropna(axis=0, how='all') if (dna) else res
-
-
-""" ********** NAME MAPPER FUNCTIONS ********** """
-substr_ad_initial_map = partial(substr_ad_map, check_fn=all_equal, accord_fn=first_element, discord_fn=first_letter_concat)
+from mutate.tfactory_util import single_row, single_row_map
+from mutate.tfactory_util import statistic, difference, fracdiff
+from mutate.tfactory_util import window_rank, normalize, symbolize
+from mutate.tfactory_util import returnify, expanding_returnify, variable_expanding_returnify
 
 
 """ ********** JSON-STR-TO-CODE TRANSLATORS ********** """
 RUNT_FN_TRANSLATOR = {
+	"sr": single_row,
+	"srm": single_row_map,
+	"stat": statistic,
 	"diff": difference,
-	"ret": returnify,
-	"retx": expanding_returnify,
-	"vretx": variable_expanding_returnify,
 	"ffd": fracdiff,
 	"wr": window_rank,
 	"norm": normalize,
 	"sym": symbolize,
-	"srf": single_row_filter,
-	"sgn": signify
+	"ret": returnify,
+	"xret": expanding_returnify,
+	"vxret": variable_expanding_returnify
 }
 
 RUNT_TYPE_TRANSLATOR = {
-	"rut": apply_rt_df,
+	"rut": apply_rut_df,
+	"rbt": apply_rbt_df,
 	"gbt": apply_gbt_df,
-	"gua": apply_gagg_df,
-	"rbt": apply_btw_df
+	"gua": apply_gua_df
 }
 
 RUNT_NMAP_TRANSLATOR = {
@@ -83,7 +63,7 @@ RUNT_FREQ_TRANSLATOR = {
 }
 
 
-""" ********** RUNT Exceptions ********** """
+""" ********** RUNT EXCEPTION CLASSES ********** """
 class RUNTFormatError(Exception):
 	"""
 	Use this class for formatting/validation errors in Runt Transforms.
@@ -97,4 +77,61 @@ class RUNTComputeError(RuntimeError):
 	"""
 	def __init__(self, message):
 		super().__init__(message)
+
+
+""" ********** HELPER FNS ********** """
+def get_ser_fn(var, ser_fn_str, SER_FNS=RUNT_FN_TRANSLATOR):
+	ser_fn = list(map(lambda fn: SER_FNS.get(fn), ser_fn_str)) if (is_type(ser_fn_str, list)) [SER_FNS.get(ser_fn_str)]
+	var = var if (is_type(var, list)) else [var]
+
+	if (len(ser_fn)!=len(var)):
+		error_msg 'number of series functions and variant sets must be equal'
+		logging.error(error_msg)
+		raise RUNTFormatError(error_msg)
+	return [partial(ser_fn[i], var[i]) for i in len(var)]
+
+
+""" ********** ROW BASED TRANSFORMS ********** """
+def apply_rut_df(df, var, freq, ser_fn_str, col_fn_str, dna=True):
+	"""
+	Apply row unary transform
+	"""
+	ser_fn = get_ser_fn(var, ser_fn_str)
+	d = {col: df[col].transform(ser_fn[i%len(ser_fn)]) for i, col in enumerate(df.columns)}
+	res = pd.DataFrame(d, index=df.index, )
+	# TODO
+	#res = df.transform(ser_transform_fn)
+	return res.dropna(axis=0, how='all') if (dna) else res
+
+def apply_rbt_df(df, var, freq, ser_fn_str, col_fn_str, dna=True):
+	"""
+	Apply row binary transform
+	"""
+	res = pd.DataFrame(index=df.index)
+	for col_a, col_b in window_iter(df.columns):
+		res.loc[:, name_map_fn(col_a, col_b)] = binary_apply_fn(df[col_a], df[col_b])
+	return res.dropna(axis=0, how='all') if (dna) else res
+
+
+""" ********** GROUP BASED TRANSFORMS ********** """
+def apply_gut_df(df, var, freq, ser_fn_str, col_fn_str, dna=True):
+	"""
+	Apply groupby unary transform
+	"""
+	# TODO
+	res = df.groupby(pd.Grouper(freq=agg_freq)).transform(ser_transform_fn)
+	return res.dropna(axis=0, how='all') if (dna) else res
+
+def apply_gua_df(df, var, freq, ser_fn_str, col_fn_str, dna=True):
+	"""
+	Apply groupby unary aggregation
+	"""
+	# TODO
+	res = df.groupby(pd.Grouper(freq=agg_freq)).agg(ser_agg_fn)
+	return res.dropna(axis=0, how='all') if (dna) else res
+
+
+""" ********** COL (NAME MAPPER) FUNCTIONS ********** """
+substr_ad_initial_map = partial(substr_ad_map, check_fn=all_equal, accord_fn=first_element, discord_fn=first_letter_concat)
+
 
--- mutate/tfactory_util.py
+++ mutate/tfactory_util.py
@@ -5,6 +5,7 @@
 #  \__/_/  \__,_/\___/\__/\____/_/   \__, /
 #                                   /____/
 # transforms factory utilities module.
+# second-order data transform functions.
 """
 Kevin Patel
 """
@@ -23,14 +24,55 @@ from mutate.pattern_util import gaussian_breakpoints, uniform_breakpoints, get_s
 from mutate.fracdiff_util import get_weights
 
 
-""" ********** TRANSFORMS ********** """
+""" ********** sr **********"""
+DEFAULT_IDX_NORM = False	# Default to return index value instead of normalized score
+DEFAULT_FNZ_IDX_SHF = 1		# Default to return index shifted by +1
+
+def first_nonzero(ser, ret_idx=DEFAULT_RET_IDX, idx_norm=DEFAULT_IDX_NORM):
+	idx = arr_nonzero(ser.values, ret_idx=ret_idx, idx_norm=idx_norm, idx_shf=DEFAULT_FNZ_IDX_SHF)
+	return idx if (not isinstance(idx, np.ndarray) and (isnt(idx) or idx==0)) return idx.item(0)
+
+ROW_IDX_SELECTOR_MAP = {
+	0: (lambda ser: ser.first_valid_index()),
+	-1: (lambda ser: ser.last_valid_index()),
+	'h': (lambda ser: ser.idxmax(skipna=True)),
+	'l': (lambda ser: ser.idxmin(skipna=True)),
+	'fnz': partial(first_nonzero, ret_idx=True, idx_norm=False),
+	'fnz_idxscore': partial(first_nonzero, ret_idx=True, idx_norm=True)	# Returns index as normalized score
+}
+ROW_VAL_SELECTOR_MAP = {
+	0: (lambda ser: ser.loc[ROW_IDX_SELECTOR_MAP.get(0)(ser)]),
+	-1: (lambda ser: ser.loc[ROW_IDX_SELECTOR_MAP.get(-1)(ser)]),
+	'h': (lambda ser: ser.loc[ROW_IDX_SELECTOR_MAP.get('h')(ser)]),
+	'l': (lambda ser: ser.loc[ROW_IDX_SELECTOR_MAP.get('l')(ser)]),
+	'fnz': partial(first_nonzero, ret_idx=False, idx_norm=False)
+}
+def single_row(val, flt):
+	"""
+	Constructs function that returns index or value for selected row.
+	"""
+	return {
+		bool(not val): ROW_IDX_SELECTOR_MAP.get(flt, None),
+		val: ROW_VAL_SELECTOR_MAP.get(flt, None)
+	}.get(True)
+
+
+""" ********** srm **********"""
+SER_MAP_FN = {
+	'nz_inv': apply_nz_nn(one_minus),
+	'sgn': np.sign
+}
+def single_row_map(val, flt, map_fn):
+	return compose(single_row(val, flt), SER_MAP_FN.get(map_fn))
+
+
+""" ********** stat **********"""
 STAT_FN_MAP = {
 	'avg': pd.Series.mean,
 	'std': pd.Series.std,
 	'max': pd.Series.max,
 	'min': pd.Series.min
 }
-
 def statistic(stat_type, abs_val=True, agg_freq='cal_daily'):
 	agg_freq = RUNT_FREQ_TRANSLATOR.get(agg_freq)
 	stat_fn = STAT_FN_MAP.get(stat_type)
@@ -44,110 +86,15 @@ def statistic(stat_type, abs_val=True, agg_freq='cal_daily'):
 
 	return fn
 
+
+""" ********** diff **********"""
 def difference(num_periods):
 	def fn(ser):
 		return ser.diff(periods=num_periods)
 	return fn
 
-RANK_FN_MAP = {
-	'zn': (lambda ser: (ser.iloc[-1]-ser.mean()) / ser.std()),
-	'mx': (lambda ser: 2 * ((ser.iloc[-1]-ser.min()) / (ser.max()-ser.min())) - 1),
-	'od': (lambda ser, normalize=True: ser.rank(numeric_only=True, ascending=True, pct=normalize).iloc[-1]),
-	'pt': (lambda ser: (ser.iloc[-1]-ser.min()) / (ser.max()-ser.min()))
-}
 
-def window_rank(rank_type, num_periods):
-	rank_fn = RANK_FN_MAP.get(rank_type)
-
-	def fn(ser):
-		win = ser.expanding(min_periods=1) if (num_periods==-1) else ser.rolling(window=num_periods, min_periods=1)
-		return win.apply(rank_fn)
-
-	return fn
-
-def normalize(norm_type):
-	norm_fn = RANK_FN_MAP.get(norm_type)
-
-	def fn(ser):
-		return ser.transform(norm_fn)
-
-	return fn
-
-RETURN_FN_MAP = {
-	"spread": (lambda slow_ser, fast_ser: fast_ser - slow_ser),
-	"ret": (lambda slow_ser, fast_ser: (fast_ser / slow_ser) - 1),
-	"logret": (lambda slow_ser, fast_ser: np.log(fast_ser / slow_ser))
-}
-
-def returnify(ret_type, thresh=None, clip=False):
-	"""
-	Spread, regular return, or log return.
-	"""
-	ret_fn = RETURN_FN_MAP.get(ret_type)
-
-	def fn(slow_ser, fast_ser):
-		ret = ret_fn(slow_ser, fast_ser)
-		if (is_valid(thresh)):
-			ret = ser_range_center_clip(ret, thresh, inner=SIDEWAYS, outer=clip, inclusive=False)
-		return ret
-
-	return fn
-
-def expanding_returnify(ret_type, thresh=None, clip=False, agg_freq='cal_daily'):
-	"""
-	Expanding spread, regular return, or log return.
-	"""
-	ret_fn = returnify(ret_type, thresh=thresh, clip=clip)
-	agg_freq = RUNT_FREQ_TRANSLATOR.get(agg_freq)
-
-	def fn(slow_ser, fast_ser):
-		first_slow = slow_ser.dropna().groupby(pd.Grouper(freq=agg_freq)).transform(single_row_filter('f'))
-		return ret_fn(first_slow, fast_ser.dropna())
-
-	return fn
-
-def variable_expanding_returnify(ret_type, stat_type, clip=False, thresh_scalar=1, agg_freq='cal_daily'):
-	"""
-	Expanding return thresholded on past period statistic.
-	"""
-	ret_fn = expanding_returnify(ret_type, thresh=None, clip=False, agg_freq=agg_freq)
-	stat_fn = statistic(stat_type, abs_val=True, agg_freq=agg_freq)
-	agg_freq = RUNT_FREQ_TRANSLATOR.get(agg_freq)
-
-	def fn(slow_ser, fast_ser):
-		ret = ret_fn(slow_ser, fast_ser)
-		stat = stat_fn(ret) * thresh_scalar
-		thresh = pd_slot_shift(pd.DataFrame({0:-stat, 1: stat}, index=ret.index), periods=1, freq=agg_freq)
-		idx = ret.index & thresh.index
-		clipped = ser_range_center_clip(ret.loc[idx], thresh.loc[idx], inner=SIDEWAYS, outer=clip, inclusive=False)
-		return clipped
-
-	return fn
-
-def sign(val):
-	if (val is None):
-		return None
-	else:
-		return {
-			val > 0: 1,
-			val < 0: -1,
-			val == 0: 0
-		}.get(True)
-
-def signify():
-	"""
-	Map a series through the sign function.
-	"""
-	def sgn(ser):
-		return ser.copy().map(sign, na_action='ignore')
-	return sgn
-
-def binary_sign_difference_count():
-	"""
-	Return the difference in positive versus negative signs divided by all.
-	"""
-	pass
-
+""" ********** ffd **********"""
 def fracdiff(d, thresh=None, size=None):
 	"""
 	Fractional Differencing
@@ -160,11 +107,39 @@ def fracdiff(d, thresh=None, size=None):
 		return ser.dropna().rolling(window=len(weights), min_periods=len(weights)).apply(dot_weights_fn)
 	return fn
 
+
+""" ********** wr **********"""
+RANK_FN_MAP = {
+	'zn': (lambda ser: (ser.iloc[-1]-ser.mean()) / ser.std()),
+	'mx': (lambda ser: 2 * ((ser.iloc[-1]-ser.min()) / (ser.max()-ser.min())) - 1),
+	'od': (lambda ser, normalize=True: ser.rank(numeric_only=True, ascending=True, pct=normalize).iloc[-1]),
+	'pt': (lambda ser: (ser.iloc[-1]-ser.min()) / (ser.max()-ser.min()))
+}
+def window_rank(rank_type, num_periods):
+	rank_fn = RANK_FN_MAP.get(rank_type)
+
+	def fn(ser):
+		win = ser.expanding(min_periods=1) if (num_periods==-1) else ser.rolling(window=num_periods, min_periods=1)
+		return win.apply(rank_fn)
+
+	return fn
+
+
+""" ********** norm **********"""
+def normalize(norm_type):
+	norm_fn = RANK_FN_MAP.get(norm_type)
+
+	def fn(ser):
+		return ser.transform(norm_fn)
+
+	return fn
+
+
+""" ********** sym **********"""
 SYM_MAP = {
 	"gau": gaussian_breakpoints,
 	"uni": uniform_breakpoints
 }
-
 def symbolize(sym_type, num_sym, numeric_symbols=True):
 	"""
 	Return symbolization encoder.
@@ -191,75 +166,57 @@ def symbolize(sym_type, num_sym, numeric_symbols=True):
 	return fn
 
 
-""" ********** FILTERS ********** """
-DEFAULT_RET_IDX = False 	# Default to return value instead of index
-DEFAULT_IDX_NORM = False	# Default to return index value instead of normalized score
-DEFAULT_FNZ_IDX_SHF = 1		# Default to return index shifted by +1
-
-def row_first(ser, ret_idx=DEFAULT_RET_IDX):
-	idx = ser.first_valid_index()
-
-	if (isnt(idx)):
-		return None
-	elif (or ret_idx):
-		return idx
-	else:
-		return ser.loc[idx]
-
-def row_last(ser, ret_idx=DEFAULT_RET_IDX):
-	idx = ser.last_valid_index()
-
-	if (isnt(idx)):
-		return None
-	elif (or ret_idx):
-		return idx
-	else:
-		return ser.loc[idx]
-
-def first_nonzero(ser, ret_idx=DEFAULT_RET_IDX, idx_norm=DEFAULT_IDX_NORM):
-	idx = arr_nonzero(ser.values, ret_idx=ret_idx, idx_norm=idx_norm, idx_shf=DEFAULT_FNZ_IDX_SHF)
-
-	if (not isinstance(idx, np.ndarray) and (isnt(idx) or idx==0)):
-		return idx
-	else:
-		return idx.item(0)
-
-def nth_only(ser, ret_idx=DEFAULT_RET_IDX):
-	if (ret_idx):
-		return ser.nth(specifier).index
-	else:
-		return ser.nth(specifier)
-
-SRF_SPECIFIER = {
-	0: first_only,
-	-1: last_only,
-	'h': None,
-	'l': None,
-	'fnz': first_nonzero,
-	'fnzi_normed': partial(first_nonzero, ret_idx=True, idx_norm=True),
-	None: nth_only
+""" ********** ret **********"""
+RETURN_FN_MAP = {
+	"spread": (lambda slow_ser, fast_ser: fast_ser - slow_ser),
+	"ret": (lambda slow_ser, fast_ser: (fast_ser / slow_ser) - 1),
+	"logret": (lambda slow_ser, fast_ser: np.log(fast_ser / slow_ser))
 }
-
-SRF_MAP_FN = {
-	'nz_inv': apply_nz_nn(one_minus),
-	'sgn': sign,
-	None: identity_fn
-}
-
-def single_row_filter(specifier, map_fn=None):
+def returnify(ret_type, thresh=None, clip=False):
 	"""
-	Return a function that filters a series down to one row and is mapped through
-	a mapping function.
-
-	Args:
-		specifier (str): type of filter to use
-		map_fn (str): mapping function to use
-
-	Return:
-		filter map function
+	Spread, regular return, or log return.
 	"""
-	flt = SRF_SPECIFIER.get(specifier, nth_only)
-	mp = SRF_MAP_FN.get(map_fn, identity_fn)		# TODO - fix
-	fn = compose(flt, mp)
+	ret_fn = RETURN_FN_MAP.get(ret_type)
+
+	def fn(slow_ser, fast_ser):
+		ret = ret_fn(slow_ser, fast_ser)
+		if (is_valid(thresh)):
+			ret = ser_range_center_clip(ret, thresh, inner=SIDEWAYS, outer=clip, inclusive=False)
+		return ret
+	return fn
+
+
+""" ********** xret **********"""
+def expanding_returnify(ret_type, thresh=None, clip=False, agg_freq='cal_daily'):
+	"""
+	Expanding spread, regular return, or log return.
+	"""
+	ret_fn = returnify(ret_type, thresh=thresh, clip=clip)
+	agg_freq = RUNT_FREQ_TRANSLATOR.get(agg_freq)
+
+	def fn(slow_ser, fast_ser):
+		first_slow = slow_ser.dropna().groupby(pd.Grouper(freq=agg_freq)).transform(single_row_filter('f'))
+		return ret_fn(first_slow, fast_ser.dropna())
+
+	return fn
+
+
+""" ********** vxret **********"""
+def variable_expanding_returnify(ret_type, stat_type, clip=False, thresh_scalar=1, agg_freq='cal_daily'):
+	"""
+	Expanding return thresholded on past period statistic.
+	"""
+	ret_fn = expanding_returnify(ret_type, thresh=None, clip=False, agg_freq=agg_freq)
+	stat_fn = statistic(stat_type, abs_val=True, agg_freq=agg_freq)
+	agg_freq = RUNT_FREQ_TRANSLATOR.get(agg_freq)
+
+	def fn(slow_ser, fast_ser):
+		ret = ret_fn(slow_ser, fast_ser)
+		stat = stat_fn(ret) * thresh_scalar
+		thresh = pd_slot_shift(pd.DataFrame({0:-stat, 1: stat}, index=ret.index), periods=1, freq=agg_freq)
+		idx = ret.index & thresh.index
+		clipped = ser_range_center_clip(ret.loc[idx], thresh.loc[idx], inner=SIDEWAYS, outer=clip, inclusive=False)
+		return clipped
+
 	return fn
 
