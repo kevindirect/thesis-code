df34b4aecbb67cbd4a163f96e432ce541950f184
kpatel28 <504052-kpatel28@users.noreply.gitlab.com>
2019-09-05 18:01:43 -0700

    update for new DataAPI and refactoring

--- recon/dataset_util.py
+++ recon/dataset_util.py
@@ -4,21 +4,21 @@ Kevin Patel
 import sys
 import os
 import logging
+from functools import reduce
 from itertools import product
 
+import numpy as np
 import pandas as pd
-from dask import delayed
 
-from common_util import DT_HOURLY_FREQ, DT_CAL_DAILY_FREQ, NestedDefaultDict, load_json, list_get_dict, list_set_dict, remove_dups_list
+from common_util import DT_HOURLY_FREQ, DT_CAL_DAILY_FREQ, isnt, is_valid, NestedDefaultDict, load_json, list_get_dict, list_set_dict, remove_dups_list
 from data.data_api import DataAPI
-from data.access_util import df_getters as dg, col_subsetters2 as cs2
 from recon.common import DATASET_DIR
 
 
 no_constraint = lambda *a: True
 asset_match = lambda a, b: a[0]==b[0]
 src_match = lambda a, b: a[2]==b[2]
-parent_match = lambda a, b: a[-1].split('_')[:-1]==b[-1].split('_')[:-1] # Common parent if all the items in desc except last are identical 
+parent_match = lambda a, b: a[-1].split('_')[:-1]==b[-1].split('_')[:-1] # Common parent if all the items in desc except last are identical
 
 flr_asset_match = lambda fp, lp, rp: asset_match(fp, lp) and asset_match(fp, rp)
 flr_src_match = lambda fp, lp, rp: src_match(fp, rp)
@@ -56,7 +56,7 @@ def gen_group(dataset, group=['features', 'labels', 'targets', 'row_masks'], out
 		Example:
 			gen_group(dataset, group=['features', 'labels'], constraint=asset_match) -> yields feature label pairs where the first items of their paths match
 	"""
-	if (constraint is None):
+	if (isnt(constraint)):
 		constraint = no_constraint
 
 	parts = [list(dataset[part][out[0]].keys()) for part in group] # Can use out[0] becuase keys are guaranteed to be identical aross all outputs
@@ -70,37 +70,31 @@ def prep_dataset(dataset_dict, assets=None, filters_map=None, dataset_dir=DATASE
 	Return the tree of lazy loaded data specified by dataset_dict, asset list, and filter mapping.
 	"""
 	dataset = {}
+	if (isinstance(dataset_dict, str)):
+		dataset_dict = load_json(dataset_dict, dir_path=dataset_dir)
 
 	for partition, accessors in dataset_dict.items():
 		if (isinstance(accessors, str)): # Dataset entry depends on another dataset, key in this and other must match
 			accessors = load_json(accessors, dir_path=dataset_dir)[partition]
-		filters = filters_map[partition] if (filters_map is not None and partition in filters_map) else None
+		filters = filters_map[partition] if (is_valid(filters_map) and partition in filters_map) else None
 		dataset[partition] = prep_data(accessors, assets=assets, filters=filters)
 
 	return dataset
 
 def prep_data(accessors, assets=None, filters=None):
 	recs, dfs = NestedDefaultDict(), NestedDefaultDict()
-
-	for au in accessors:
-		for path, rec, df in DataAPI.lazy_yield(list_get_dict(dg, au), list_get_dict(cs2, au)):
-			if ((assets is None or path[0] in assets) and filters_match(path, filter_lists=filters)):
-				recs[path], dfs[path] = rec, df
-
-	# Assert there are no duplicate path keys
-	assert(len(remove_dups_list([''.join(lst) for lst in recs.keys()]))==len(recs))
-	assert(len(remove_dups_list([''.join(lst) for lst in dfs.keys()]))==len(dfs))
+	flt = lambda p: (isnt(assets) or p[0] in assets) and filters_match(p, filter_lists=filters)
+	for axe in accessors:
+		for path, rc, df in DataAPI.axe_yield(axe, flt=flt, lazy=True):
+			recs[path], dfs[path] = rc, df
+	#recs, dfs = reduce(lambda a, b: (a[0]+b[0], a[1]+b[1]), [DataAPI.axe_load(axe, flt=flt, lazy=True) for axe in accessors])
 	return {'recs': recs, 'dfs': dfs}
 
 def filters_match(item, filter_lists=None):
 	def filter_match(item, filter_list):
 		for idx, filter_field in enumerate(filter_list):
-			if (filter_field is not None and item[idx]!=filter_field):
+			if (is_valid(filter_field) and item[idx]!=filter_field):
 				return False
 		else:
 			return True
-
-	if (filter_lists is not None):
-		return any(filter_match(item, filter_list) for filter_list in filter_lists)
-	else:
-		return True
+	return True if (isnt(filter_lists)) else any(filter_match(item, flt) for flt in filter_lists)
