2352b481d575cfe8604d2ac6cf2258993ed2c657
kpatel28 <504052-kpatel28@users.noreply.gitlab.com>
2019-10-08 18:10:35 -0700

    small updates to recon tools

--- recon/gta_util.py
+++ recon/gta_util.py
@@ -7,11 +7,12 @@ from os import sep
 from os.path import splitext
 import logging
 
+import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA, KernelPCA, FactorAnalysis
 
-from common_util import DT_HOURLY_FREQ, DT_CAL_DAILY_FREQ, inner_join, zdiv, benchmark
+from common_util import DT_HOURLY_FREQ, DT_CAL_DAILY_FREQ, is_valid, ser_shift, inner_join, zdiv, benchmark
 from recon.common import REPORT_DIR
-from recon.label_util import shift_label
 
 
 """ ********** APPLY FUNCTIONS ********** """
@@ -31,10 +32,10 @@ def feat_label_apply(feat_df, feat_df_desc, label_df, apply_fn, label_shift=-1):
 	for feat in feat_df.columns:
 		row = {}
 		row['feat_col_name'] = feat
-		row.update({label: apply_fn(feat_df[feat], shift_label(label_df[label], shift_periods=label_shift)) for label in label_df.columns})
+		row.update({label: apply_fn(feat_df[feat], ser_shift(label_df[label], shift_periods=label_shift)) for label in label_df.columns})
 		result = result.append(row, ignore_index=True)
 
-	if (feat_df_desc is not None):
+	if (is_valid(feat_df_desc)):
 		result.insert(0, 'feat_df_desc', feat_df_desc)
 
 	return result
@@ -53,11 +54,11 @@ def count(a, b, method='ratio_count'):
 	Return count of rows of a relative to b (or non-null count of a if count method is selected).
 
 	Examples:
-	a = pd.Series([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], name='a')										# 10 non-null
-	b = pd.Series([0, 1, None,	3, 4, None, 6, 7, 8, 9], name='b')								# 8 non-null
+	a = pd.Series([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], name='a')						# 10 non-null
+	b = pd.Series([0, 1, None, 3, 4, None, 6, 7, 8, 9], name='b')					# 8 non-null
 	c = pd.Series([None, None, None, None, None, None, None, None, None, None], name='c')		# 0 non-null
-	d = pd.Series([0, 1, 2, 3,	4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], name='d')				# 15 non-null
-	e = pd.Series([0, 1, 2, None, 4, None, 6, 7, 8, 9, 10, 11, 12, None, 14], name='e')			# 12 non-null
+	d = pd.Series([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], name='d')			# 15 non-null
+	e = pd.Series([0, 1, 2, None, 4, None, 6, 7, 8, 9, 10, 11, 12, None, 14], name='e')		# 12 non-null
 
 	count(a, b) # expected 1
 	count(b, a) # expected .8
@@ -87,6 +88,27 @@ def count(a, b, method='ratio_count'):
 	elif (method == 'int_count'):
 		return a.count()
 
+def pca(a, b, num_comp):
+	"""
+	Principal Component Analysis
+	"""
+	trf = PCA(n_components=num_comp, copy=True, whiten=False)
+	return trf.fit_transform(a)
+
+def kpca(a, b, num_comp, kernel, **kwargs):
+	"""
+	Kernel Principal Component Analysis
+	"""
+	trf = KernelPCA(n_components=num_comp, kernel=kernel, **kwargs)
+	return trf.fit_transform(a)
+
+def factor_analysis(a, b, num_comp):
+	"""
+	Factor Analysis
+	"""
+	trf = FactorAnalysis(n_components=num_comp, copy=True)
+	return trf.fit_transform(a)
+
 
 """ ********** MISC UTIL ********** """
 report_path_dir = lambda dataset_fname, asset: sep.join([REPORT_DIR, splitext(dataset_fname)[0], asset]) +sep
@@ -99,5 +121,8 @@ GTA_TYPE_TRANSLATOR = {
 
 GTA_TEST_TRANSLATOR = {
 	"corr": corr,
-	"count": count
+	"count": count,
+	"pca": pca,
+	"kpca": kpca,
+	"factor_analysis": factor_analysis
 }
